1ee652ae3de68fed089252adb4b237a200be05ff
dp <davepollan@gmail.com>
2020-02-05 19:49:35 -0500

    first test passed

---
+++ index.js
@@ -0,0 +1,17 @@
+const questions = ['Which Employee Type Do You Want to Add?', "Enter Your Name.", "Enter Your Email.", "Enter Your ID No."];
+
+const Employee = require("./lib/Employee");
+
+// Initialize a new Game object
+const employee = new Employee();
+
+employee.start();
+
+
+
+
+
+
+
+
+

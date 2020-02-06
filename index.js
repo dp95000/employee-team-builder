471bdab2d865781e0946d8899fc5979f9264fd58
dp <davepollan@gmail.com>
2020-02-06 00:56:06 -0500

    object creator function done

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const questions = ['Which Employee Type Do You Want to Add?', "Enter Your Name.", "Enter Your Email.", "Enter Your ID No."];
+const Main = require('./lib/index');
 
-const Employee = require("./lib/Employee");
-
-// Initialize a new Game object
-const employee = new Employee();
-
-employee.start();
+const program = new Main();
+program.run();
 
 
 
